9220129f788d483ae5543cc4ad6830781c66eed2
threepebbles <threepebbles@naver.com>
2025-03-08 19:24:23 +0900

    test: 미션1 완료

--- src/test/java/ListStudy.java
+++ src/test/java/ListStudy.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class ListStudy {
     @Test
     public void arrayList() {
-        SimpleArrayList<String> values = new SimpleArrayList<>();
+        SimpleList<String> values = new SimpleArrayList<>();
         values.add("first");
         values.add("second");
 
@@ -16,7 +16,26 @@ public class ListStudy {
         assertThat(values.remove(0)).isEqualTo("first"); // 첫 번째 값을 삭제한다.
         assertThat(values.size()).isEqualTo(2); // 값이 삭제 됐는지 확인한다.
 
-        // TODO values에 담긴 모든 값을 출력한다.
+        // TODO: values에 담긴 모든 값을 출력한다.
+        for (int i = 0; i < values.size(); i++) {
+            System.out.println(values.get(i));
+        }
+    }
+
+    @Test
+    void mission1() {
+        SimpleList<Integer> values = new SimpleArrayList<>();
+        values.add(1);
+        values.add(2);
+
+        assertThat(values.add(3)).isTrue(); // 세 번째 값을 추가한다.
+        assertThat(values.size()).isEqualTo(3); // list의 크기를 구한다.
+        assertThat(values.get(0)).isEqualTo(1); // 첫 번째 값을 찾는다.
+        assertThat(values.contains(1)).isTrue(); // "first" 값이 포함되어 있는지를 확인한다.
+        assertThat(values.remove(0)).isEqualTo(1); // 첫 번째 값을 삭제한다.
+        assertThat(values.size()).isEqualTo(2); // 값이 삭제 됐는지 확인한다.
+
+        // TODO: values에 담긴 모든 값을 출력한다.
         for (int i = 0; i < values.size(); i++) {
             System.out.println(values.get(i));
         }
